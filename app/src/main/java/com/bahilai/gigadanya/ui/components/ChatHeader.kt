506c0bcc6b4c35e26aee843fe7079050646174ad
Bahilai <vasyafedorov84@gmail.com>
2025-11-07 13:44:33 +1000

    Third day start

--- app/src/main/java/com/bahilai/gigadanya/ui/components/ChatHeader.kt
+++ app/src/main/java/com/bahilai/gigadanya/ui/components/ChatHeader.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
